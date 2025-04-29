cc79964d1e38b9be4484e745ce0cbaa0d3081a94
Cande <165179894+MCandela1@users.noreply.github.com>
2025-04-29 07:41:16 -0300

    Add files via upload

---
+++ Ejer2POO/Ejer2POO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer2POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+          Cuadrado cuad = new Cuadrado();
+
+          Console.WriteLine("El Color seleccionado fue: " + cuad.color);
+
+          cuad.lado = 20;
+          Console.WriteLine("El Lado mide: " + cuad.lado);
+
+          Console.WriteLine("El Perimetro mide: " + cuad.perimetro);
+          */
+
+            Humano h1 = new Humano();
+            h1.fechanac = new DateTime(1990, 10, 17);
+            Console.WriteLine($"Edad {h1.edad}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EjerPOO1/Bolilla.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerPOO1
+{
+    internal class Bolilla
+    {
+        private int _numero;
+        private string _color;
+
+        public int numero
+        {
+            get { return this._numero; }
+            set { this._numero = value; }
+        }
+
+        public string color
+        {
+            get { return this._color; }
+            set { 
+                if (value == "NEGRO" || value == "BLANCO" || value == "ROJO")
+                {
+                    this._color = value;
+                }
+            }
+        }
+    }
+}
